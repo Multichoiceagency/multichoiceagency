1144285d0b9f6f0514277be0291f365b8cf56137
Multichoiceagency <139119345+Multichoiceagency@users.noreply.github.com>
2024-10-07 22:15:37 +0200

    Git commit resonance

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['pbs.twimg.com', 'aceternity.com'],
-      
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
